f79fee7fada50440c8646b81eab81d23cbaf46aa
omprakashb1236 <omprakashb1236@gmail.com>
2024-08-22 13:12:33 +0530

    fav icons updated

--- pages/index.js
+++ pages/index.js
@@ -17,22 +17,21 @@ export default function Home({ homeassets }) {
         <title>Riddara</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/fav/apple-icon-57x57.png"/>
-<link rel="apple-touch-icon" sizes="60x60" href="/fav/apple-icon-60x60.png"/>
-<link rel="apple-touch-icon" sizes="72x72" href="/fav/apple-icon-72x72.png"/>
-<link rel="apple-touch-icon" sizes="76x76" href="/fav/apple-icon-76x76.png"/>
-<link rel="apple-touch-icon" sizes="114x114" href="/fav/apple-icon-114x114.png"/>
-<link rel="apple-touch-icon" sizes="120x120" href="/fav/apple-icon-120x120.png"/>
-<link rel="apple-touch-icon" sizes="144x144" href="/fav/apple-icon-144x144.png"/>
-<link rel="apple-touch-icon" sizes="152x152" href="/fav/apple-icon-152x152.png"/>
-<link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-icon-180x180.png"/>
-<link rel="icon" type="image/png" sizes="192x192"  href="/fav/android-icon-192x192.png"/>
-<link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png"/>
-<link rel="icon" type="image/png" sizes="96x96" href="/fav/favicon-96x96.png"/>
-<link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png"/>
-<link rel="manifest" href="/fav/manifest.json"/>
-
-<meta name="msapplication-TileImage" content="/fav/ms-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="57x57" href="/fav/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/fav/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/fav/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/fav/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/fav/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/fav/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/fav/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/fav/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/fav/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/fav/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/fav/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/fav/favicon-16x16.png" />
+        <meta name="theme-color" content="#f7f4ee"/>
+        <meta name="msapplication-TileImage" content="/fav/ms-icon-144x144.png" />
 
       </Head>
 
@@ -61,7 +60,7 @@ export default function Home({ homeassets }) {
 
         </div>
         <div className='content-image'>
-          
+
 
           {homeassets[0].main_content_image && (
             <Image
@@ -88,7 +87,7 @@ export default function Home({ homeassets }) {
           </div>
 
           <div className='footer-logo'>
-            
+
             {homeassets[0].footer_logo && (
               <Image
                 src={urlFor(homeassets[0].footer_logo.asset._ref).url()}
