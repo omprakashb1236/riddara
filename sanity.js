3e772374b238b9f4ae5e14c5845a2d7187592d0d
omprakashb1236 <omprakashb1236@gmail.com>
2024-08-29 19:40:51 +0530

    token removed

--- sanity.js
+++ sanity.js
@@ -6,7 +6,6 @@ export const sanityClient = createClient({
     dataset: "production",
     apiVersion: "2022-03-25",
     useCdn: false,
-    token:process.env.SANITY_API_TOKEN
 });
 
 const builder = imageUrlBuilder(sanityClient);
